c6302ee7a3ed7a9ba7c7eb74fb7d05d5d3a78779
Anhar Amir <anhar.amir@myboost.id>
2020-10-25 23:25:11 +0700

    wave svg animation

--- src/components/header.js
+++ src/components/header.js
@@ -3,30 +3,13 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <header>
+    <div className="Header">
+      <p>Logo</p>
+      <p>Course</p>
+      <p>Article</p>
+      <p>Signin</p>
+      <p>Register</p>
     </div>
   </header>
 )
@@ -40,3 +23,15 @@ Header.defaultProps = {
 }
 
 export default Header
+
+{
+  /* <Link
+          to="/"
+          style={{
+            color: `white`,
+            textDecoration: `none`,
+          }}
+        >
+          {siteTitle}
+        </Link> */
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,46 @@
 import React from "react"
-import { Link } from "gatsby"
-
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import imageLaptop from "../images/laptop.jpg"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
-
-export default IndexPage
+export default function Index() {
+  return (
+    <Layout>
+      <div className="Banner">
+        <img src={imageLaptop} alt="laptop" />
+        <div className="BannerText">
+          <h2>
+            Learn design and code <br /> in one place
+          </h2>
+          <p>
+            Prototype and build apps with React, React Native, Flutter, and
+            More.
+          </p>
+          <button>Watch Video</button>
+          <svg
+            width="100%"
+            height="172"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path fill="white">
+              <animate
+                repeatCount="indefinite"
+                fill="freeze"
+                dur="10s"
+                attributeName="d"
+                values="M0 25.9086C277 84.5821 433 65.736 720 25.9086C934.818 -3.9019 1214.06 -5.23669 1442 8.06597C2079 45.2421 2208 63.5007 2560 25.9088V171.91L0 171.91V25.9086Z;
+                M0 87.1596C316 87.1597 444 160 884 52.0001C1324 -55.9999 1320.29 34.966 1538 71.251C1814 117.251 2156 189.252 2560 87.1597V233.161L0 233.161V87.1596Z;
+                M0 53.6584C158 11.0001 213 0 363 0C513 0 855.555 115.001 1154 115.001C1440 115.001 1626 -38.0004 2560 53.6585V199.66L0 199.66V53.6584Z;
+                M0 25.9086C277 84.5821 433 65.736 720 25.9086C934.818 -3.9019 1214.06 -5.23669 1442 8.06597C2079 45.2421 2208 63.5007 2560 25.9088V171.91L0 171.91V25.9086Z
+                "
+              ></animate>
+            </path>
+          </svg>
+        </div>
+      </div>
+      <div className="Logo">
+        <p>Logo</p>
+      </div>
+    </Layout>
+  )
+}
